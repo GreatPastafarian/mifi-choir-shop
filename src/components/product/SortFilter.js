a3d4cec460ea96db24a488f70cd741be398512f0
vasilisiy <vasilisiy0703@yandex.ru>
2025-08-19 19:51:04 +0300

    v_0_2_3

--- src/components/product/SortFilter.js
+++ src/components/product/SortFilter.js
@@ -10,8 +10,8 @@ function SortFilter({ onSortChange }) {
         onChange={(e) => onSortChange(e.target.value)}
         >
         <option value="default">Без сортировки</option>
-        <option value="price_asc">По цене: от низкой к высокой</option>
-        <option value="price_desc">По цене: от высокой к низкой</option>
+        <option value="price_asc">По сумме взноса: от низкой к высокой</option>
+        <option value="price_desc">По сумме взноса: от высокой к низкой</option>
         <option value="name_asc">По названию: от А до Я</option>
         <option value="name_desc">По названию: от Я до А</option>
         <option value="newest">По новизне</option>
